57f27cc4bd4bfcd6455e98efc8beee5c931dee66
dustyhawthorne <dusty@rangers.org>
2012-11-28 15:41:17 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bigfun::Application.config.secret_token = 'f9aaa5e9d74ea1e7ebef04f61969147a3b273240ed4f336e881c9c0a653922b32754d1fd1c25954aac0bc7aeddb4c1a28a5c323b717c4990327f42c9e3e6e657'
